cde04b8005d736bc1bd753764142d15a2af5dee5
Dev Kumar <dev2005.rohini@gmail.com>
2026-01-14 21:02:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    # This 'or' part fixes the CSRF Error by providing a backup key
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-fallback-key-123'
     DEBUG = True
     
-    # We leave these here, but they won't be used by the app right now
+    # SQL connection, they won't be used by the app, but can be used in future
     DB_HOST = os.environ.get('DB_HOST')
     DB_USER = os.environ.get('DB_USER')
     DB_PASSWORD = os.environ.get('DB_PASSWORD')
