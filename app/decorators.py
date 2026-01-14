9822e9e1aac992e459aa0301e10695397e57d1b6
Dev Kumar <dev2005.rohini@gmail.com>
2026-01-14 20:58:42 +0530

    Update decorators.py

--- app/decorators.py
+++ app/decorators.py
@@ -1,4 +1,3 @@
-# /ayursutra/app/decorators.py
 
 from functools import wraps
 from flask import session, redirect, url_for, g
@@ -9,19 +8,13 @@ def login_required(view):
     @wraps(view)
     def wrapped_view(**kwargs):
         if 'user_id' not in session:
-            # If user is not logged in, redirect to login page
             return redirect(url_for('auth.login'))
-        
-        # If user is logged in, load their info into 'g'
-        # This makes g.user available in all protected templates
         db = g.db_cursor
-        
-        # Add a check in case db_cursor is None
         if db:
             db.execute("SELECT * FROM Users WHERE id = %s", (session['user_id'],))
             g.user = db.fetchone()
         else:
-            g.user = None # or handle error
+            g.user = None
             
         return view(**kwargs)
     return wrapped_view
@@ -29,17 +22,13 @@ def login_required(view):
 def practitioner_required(view):
     @wraps(view)
     def wrapped_view(**kwargs):
-        # First, check if they are logged in at all
         if 'user_id' not in session:
             flash('Please log in to access this page.', 'warning')
             return redirect(url_for('auth.login'))
         
-        # Then, check if they are a practitioner
         if session.get('role') != 'practitioner':
             flash('You do not have permission to access this page.', 'danger')
-            return redirect(url_for('main.dashboard')) # Send them to their own dashboard
-        
-        # If they are logged in and are a practitioner, load their data
+            return redirect(url_for('main.dashboard'))
         db = g.db_cursor
         if db:
             db.execute("SELECT * FROM Users WHERE id = %s", (session['user_id'],))
